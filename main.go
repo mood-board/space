bb0240350df7ee9bcde28c31801663eeb823ee56
Ofonime Usoro <ofonimeusoro01@gmail.com>
2019-01-18 14:41:34 +0200

    Setting up application

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/mkideal/cli"
 	"github.com/ofonimefrancis/spaceship/common/config"
@@ -11,6 +12,7 @@ func main() {
 	cli.Run(new(config.PackageFlags), func(ctx *cli.Context) error {
 		argv := ctx.Argv().(*config.PackageFlags)
 		fmt.Println(argv)
+		fmt.Println(time.Now().Format(time.RFC3339))
 		return nil
 	})
 }
